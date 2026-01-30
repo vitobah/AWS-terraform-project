31ed09fd9daeaf2c5f46d5825d4bfcfe8750dff6
vitobah <onyebuchimonyema@gmail.com>
2026-01-30 02:21:14 -0600

    Format terraform files

--- main.tf
+++ main.tf
@@ -122,11 +122,11 @@ resource "aws_security_group" "alb_sg" {
 # Standalone EC2
 ############################
 resource "aws_instance" "web" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public1.id
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public1.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name      = "IBUCHIM25160508"
+  key_name               = "IBUCHIM25160508"
 
   user_data = <<-EOF
               #!/bin/bash
